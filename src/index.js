1a2776d0a5a3c0d90efb230d748884f19e049ce7
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2022-07-05 17:06:10 +0300

    ver. 2.1

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import './css/styles.css';
-import { fetchCountries } from './jss/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
